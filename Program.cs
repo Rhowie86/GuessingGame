32c05db1436c6e790bc826b8987ffe72b2dc5794
Rhowie86 <ronaldnhowie@gmail.com>
2021-03-02 14:32:03 -0600

    guessing game phase 7

--- Program.cs
+++ Program.cs
@@ -2,37 +2,175 @@
 
 guessPrompt();
 
+int difficultySetting() {
 
+     Console.WriteLine(@"
+     
+                   /~~~\
+            -/     \--\.
+          ./           `.
+        .-/             |\
+       / /                \
+    .-/                    \
+    `/                     |
+   /                      -)
+  .                        '.
+ /                          \
+ |                          |
+ |                          /
+ /                        ./
+|                     . \ |
+|                  ) \ .) )
+|                | | ). )/
+|               |) )/ ///
+ .             .)----~~~.
+ \             /~        \
+ \            /           \
+ \           /            '
+ |          |              |
+ .          |              |
+  .         |              |
+  \         |              \
+  `          \     \.       |
+   .         \      \.      |
+   \          .      \     |
+    .         \       \    |.
+    \          .       ~.  /'
+     \         \      .-.  |
+     |    -    )      ( \ .-.
+      .  . |||        . o\(o)
+      \  |('          \  )\_/
+      \   \)           ~-~| |
+      .  |)               \'
+       \  |~            .-. \)
+       `..             ./0o  \
+        \|             (___-  )
+        \|                 \~~'
+         \         .--\___  )           Select your difficulty Butthead...
+         |        /-'-(_(|~T        <       Easy, Medium, or Hard....heh heh
+         `\     |      <'~~               
+          \     \ ___   \-__.
+          |      \__---~~   )
+          |    (           /'
+          |     ~-'        |
+          |           _____'
+          |        _-~
+          |       /
+                 .'
+                 /
+     
+     ");
+    string difficulty = Console.ReadLine().ToLower();
+
+    int guesses = 0;
+
+         if (difficulty == "easy"){
+             guesses = 8;
+         } else if (difficulty == "medium") {
+             guesses = 6;
+         } else if (difficulty == "hard") {
+              guesses = 4;
+         }
+    return guesses;
+
+}
 
 void guessPrompt()
  {
-     Console.WriteLine("Guess a number");
+    int guesses = difficultySetting();
+
+
+     Console.WriteLine(@"
+     
+     
+       .------..                            _------__--___.__.
+    /            \_                       /            `  `    \
+  /                \                     |.                     \
+ /                   \                   \                       |
+/    .--._    .---.   |                   \                      |
+|  /      -__-     \   |                    ~-/--`-`-`-\         |
+| |                |  |                     |          \        |
+ ||                  ||                     |            |       |
+ ||     ,_   _.      ||                     |            |       |
+ ||      e   e      ||  Hey Beavis,         |   _--    |       |
+  ||     _  |_      ||  Pick a Number!     _| =-.    |.-.    |
+ @|     (o\_/o)     |@                       o|/o/       _.   |
+   |     _____     |                        /  ~          \ |
+    \ ( /uuuuu\ ) /                        (/___@)  ___~    |
+     \  `====='  /                           |_===~~~.`    |
+      \  -___-  /                         _______.--~     |
+       |       |            //             \________       |
+       /-_____-\       .  _//_                      \      |
+     /           \     \\/////                    __/-___-- -_
+   /               \    \   /                    /            __\
+  /__|  AC / DC  |__\   / /                      -| Metallica|| |
+  | ||           |\ \  / /                       ||          || |
+  | ||           | \ \/ /                        ||          || |
+
+
+     
+     ");
      string answer = Console.ReadLine();
      
      int num = new Random().Next(1, 100);
-     Console.WriteLine(num);
      int chance = 1;
      bool run = true;
     
-    while (chance < 5){
+    while (chance < 4){
 
      if (num == int.Parse(answer)){
          Console.WriteLine("Good guess! You nailed it! " + answer + " is the secret number!");
-         chance = 5;
+         chance = 4;
          run = false;
      } else {
-         Console.WriteLine("You guessed " + answer + ", Nope! Too bad...");
-        //  Console.WriteLine("Try Again");
+         guesses--;
+         
+         Console.WriteLine("You guessed " + answer);
+         if (num > int.Parse(answer)){
+             Console.WriteLine("Thats too low");
+         } else {
+             Console.WriteLine("Thats too high");
+         }
+         Console.WriteLine("Try Again, " + guesses + " chances left.");
          answer = Console.ReadLine();
          
          ++chance;
 
-         if (chance == 5){
+         if (chance == 4){
              run = false;
          }
      }}
         if (run == false){
-            Console.WriteLine("Game Over Man!");
+            Console.WriteLine(@"
+            
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+
+            
+            ");
         }
      
      }
